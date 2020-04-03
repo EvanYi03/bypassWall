23032fd4253beb16047fa2d3622c8c6e966434fe
nonnimartin@gmail.com <nonnimartin@gmail.com>
2020-04-02 21:32:34 -0700

    Fixing double submission

--- popup.js
+++ popup.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', documentEvents  , false);
 
 function myAction(input) { 
-    console.log("input value is : " + input.value);
+    console.log('input value is : ' + input.value);
+    if (input.value == ''){
+      alert('Empty string entered');
+      return;
+    } 
+    if (!input.value.startsWith('http')){
+      alert('No protocol specified');
+      return;
+    }
     alert("The entered data is : " + input.value);
 }
 
