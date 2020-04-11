280735b97fc768600e5015061e375dedf11afc1b
nonnimartin@gmail.com <nonnimartin@gmail.com>
2020-04-11 00:36:01 -0700

    Removed unneeded logging

--- content.js
+++ content.js
@@ -1,19 +1,14 @@
 $(document).ready(function () {
 
-  //replace this with persisted list
-  //urlsArray = ["https://www.nytimes.com"];
   var urlsArray = [];
   chrome.storage.local.get(['urlsArray'], function(result) {
-    console.log('urls array = ' + JSON.stringify(result));
-     urlsArray = result['urlsArray'];
+    //add a "." to any page that is in the stored list of urls
+    urlsArray = result['urlsArray'];
 
      for (thisUrl in urlsArray){
       var currentLocation = location.href.toString();
-      console.log('this url = ' + urlsArray[thisUrl]);
       if (currentLocation.startsWith(urlsArray[thisUrl]) && !(currentLocation.startsWith(urlsArray[thisUrl] + "."))){
-        console.log('url got into this block = ' + urlsArray[thisUrl]);
         var baseUrl = urlsArray[thisUrl];
-        console.log('changing url to ' + baseUrl + '.');
         location.replace(baseUrl + ".");
       }
     }
