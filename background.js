fbdef2854f452e6f67abf4ddaa98959eabd711fd
nonnimartin@gmail.com <nonnimartin@gmail.com>
2020-12-20 21:02:13 -0800

    Added space to logging statement

--- background.js
+++ background.js
@@ -6,7 +6,7 @@ const getAllCookiesForTab = () => {
         chrome.cookies.getAll({domain: "nytimes.com"}, function(cookiesArray) {
             console.log('Length of Cookies Array = ' + cookiesArray.length);        
                 for(let i=0; i < cookiesArray.length; i++) {
-                  console.log('This cookie =' + cookiesArray[i].name);
+                  console.log('This cookie = ' + cookiesArray[i].name);
                 }
         });
     
