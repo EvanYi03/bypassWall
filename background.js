0158fe19c346b11c78fbdc4efb5299db9a798222
nonnimartin@gmail.com <nonnimartin@gmail.com>
2020-12-20 23:01:50 -0800

    Now returning map of cookies for url

--- background.js
+++ background.js
@@ -1,20 +1,38 @@
 const getAllCookiesForTab = () => {
     chrome.runtime.onMessage.addListener(function(message ,sender, sendResponse){
-        console.log('message', message);
+        
         const url = message.currentUrl;
-        console.log('Current tab url = ', url);
-    
-        chrome.cookies.getAll({domain: "nytimes.com"}, function(cookiesArray) {
-            console.log('Length of Cookies Array = ' + cookiesArray.length);        
-                for(let i=0; i < cookiesArray.length; i++) {
-                  console.log('This cookie = ' + cookiesArray[i].name);
-                }
-        });    
+
+        // construct array of hosts and domains to try
+        let domainsArray = new Array();
+        domainsArray.push(message.fullOrigPath);
+        domainsArray.push(message.fullWwwHost);
+        domainsArray.push(message.plainHost);
+        
+        // iterate through domains array and get cookies keys
+        // return map of cookies for each
+        let allCookiesMap = new Map();
+        for (let item in domainsArray){
+            let thisCookiesArray = getCookiesArray(domainsArray[item]);
+        // add array of cookies keys to map for each domain name
+            allCookiesMap[domainsArray[item]] = thisCookiesArray;
+        }
+        console.log(allCookiesMap);
     })
 }
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
     console.log('stored urls array', message.storedUrlsArray);   
 })
+
+const getCookiesArray = (thisUrl) => {
+    let keysArray = new Array();
+    chrome.cookies.getAll({domain: thisUrl}, function(cookiesArray) {       
+        for (let i=0; i < cookiesArray.length; i++) {
+            if (cookiesArray.length > 0) keysArray.push(cookiesArray[i].name);
+        }
+    });
+    return keysArray;
+}
 
 getAllCookiesForTab();
