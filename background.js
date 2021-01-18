bbe32d2fe22ce98fd9a2aa171aecf6fc794d4778
nonnimartin@gmail.com <nonnimartin@gmail.com>
2021-01-17 19:11:12 -0800

    Async portion now appears to be working, although it could use refactoring

--- background.js
+++ background.js
@@ -1,82 +1,107 @@
-const getAllCookiesForTab = async (message) => {
+const getCookiesArray = async (thisUrl) => {
+    return new Promise(function(resolve,reject){
+        chrome.cookies.getAll({domain: thisUrl}, function(cookies) {
+            //something may be going on here that's keeping elements from being
+            //set in the map
+            //!!!!!!!!!!!!!!!
+            console.log('cookies:', cookies);
+            resolve(cookies);
+        });
+    })
+}
 
-    const url = message.currentUrl;
+// run process on content message
+chrome.runtime.onMessage.addListener(async function(message ,sender, sendResponse){
+    
+    const storedUrlsArray = message.storedUrlsArray;
+    const currentUrl = message.currentUrl;
 
     // construct array of hosts and domains to try
     let domainsArray = new Array();
     domainsArray.push(message.fullOrigPath);
     domainsArray.push(message.fullWwwHost);
     domainsArray.push(message.plainHost);
-    
+
+    // // asyc generator (thus *)
+    // async function* createAsyncIterable(syncIterable) {
+    //     for (const elem of syncIterable) {
+    //         yield elem;
+    //     }
+    // }
+
     // iterate through domains array and get cookies keys
     // return map of cookies for each
-    let allCookiesMap = new Map();
-    for (let item in domainsArray){
-    // add array of cookies keys to map for each domain name
-        getCookiesArray(domainsArray[item]).then(thisArray => {
-            allCookiesMap.set(domainsArray[item], thisArray); 
-        });
+
+    const processArray = async (thisArray) => {
+
+        let promisesArray = new Array();
+        let allCookiesMap = new Map();
+
+        const getArrayAsync = async (thisUrl) => {  
+            return await getCookiesArray(thisUrl);
+        }
+        
+        try {
+    
+            for (const thisUrl of thisArray) {
+                const theseCookies = await getCookiesArray(thisUrl);
+                promisesArray.push(theseCookies);
+                console.log('map url', thisUrl);
+                console.log('map cookies', theseCookies);
+                allCookiesMap.set(thisUrl, theseCookies);
+                console.log('allCookiesMap', allCookiesMap);
+
+                console.log('promises array len', promisesArray.length);
+                console.log('thisarray len', thisArray.length);
+
+                //THERE'S LIKELY A BETTER WAY TO DO THIS
+                if (promisesArray.length == thisArray.length){
+                    console.log('got to this');
+                    return allCookiesMap;
+                }  
+            }
+       }catch(error){
+        console.log('ERROR', error);
+       }
     }
 
-    return allCookiesMap;
-}
+    const clearCookies = async (cookiesArray) => {
+        console.log(cookiesArray);
+    }
 
-const getCookiesArray = async (thisUrl) => {
-    let keysArray = new Array();
-    chrome.cookies.getAll({domain: thisUrl}, function(cookiesArray) {       
-        for (let i = 0; i < cookiesArray.length; i++) {
-            if (cookiesArray.length > 0) keysArray.push(cookiesArray[i].name);
-        }
-    });
-    return keysArray;
-}
-
-// run process on content message
-chrome.runtime.onMessage.addListener(async function(message ,sender, sendResponse){
-    const storedUrlsArray = message.storedUrlsArray;
-    const allCookiesMap = getAllCookiesForTab(message);
-    allCookiesMap.then(thisMap => {
-        console.log('thisMap', thisMap);
-        for (let thisUrl of thisMap.keys()) {
-            console.log('this url', thisUrl);
-            console.log('this array', thisMap.get(thisUrl));
-            let thisCookiesArray = thisMap.get(thisUrl);
-            console.log('this cookies array', thisCookiesArray);
-            // WHY WON'T EITHER OF THESE LOOPS RUN?
-            // IT SEEMS ASYNC BC THE BELOW LOG IS UNDEFINED BUT LOOK AT    
-            console.log('just to test', thisCookiesArray[0]);
-            thisCookiesArray.forEach(thisCookie => {
-                console.log(thisCookie);
-            })
-            for (i = 0; i < thisCookiesArray.length; i++){
-                console.log(thisCookiesArray[i]);
-            }
     
-        }
-    //     cookiesMapArray.forEach(thisUrl => {
-    //         console.log(thisUrl);
-    //     // let thisCookiesArray = allCookiesMap[thisUrl];
-    //     // try{
-    //     //     console.log('Array length', thisCookiesArray.length);
-    //     //     console.log('Array', thisCookiesArray);
-    //     //     thisCookiesArray.forEach(thisCookie => {
-    //     //         console.log(thisCookie);
-    //     //     })
-    //     // }catch(error){
-    //     //     throw error;
-    //     // }
-    //    })
+    console.log(processArray(domainsArray));
+
+    processArray(domainsArray).then((res) => {
+        console.log(res);
+        clearCookies(res);
     })
 
-    // WHY DO THESE STOP LOOPING?
-    // IS IT BECAUSE ARRAY HAS LENGTH OF 0?
-    // LOOK INTO MAKING ASYNC
 
+    
+    // processArray(domainsArray).then((thisMap) => {
+    //     console.log('test', thisMap);
+    //     // for (let thisUrlKey of thisMap.keys()){
+    //     //     console.log(thisUrlKey);
+    //     //     console.log(thisMap.get(thisUrlKey).length, thisMap.get(thisUrlKey));
 
-    // for (let item = 0; item < cookiesMapArray.length; item++){
-    //     let thisUrl = cookiesMapArray[item];
-    //     let thisCookiesArray = allCookiesMap[thisUrl];
-    //     console.log(thisCookiesArray);
+    //     //     if (thisMap.get(thisUrlKey)[0]) console.log(thisMap.get(thisUrlKey));
+    //     // }
+    // });
 
-    // }
+    // domainsArray.forEach((thisUrl) => {
+    // // add array of cookies keys to map for each domain name
+    //     let keysArray = new Array();
+    //      chrome.cookies.getAll({domain: thisUrl}, async function(cookiesArray) {       
+    //         for (let thisCookie of cookiesArray) {
+    //             if (cookiesArray.length > 0) keysArray.push(thisCookie.name);
+    //             console.log('thisUrl', thisUrl);
+    //             console.log('keysArray', keysArray);
+    //             allCookiesMap[thisUrl] = keysArray;
+    //         }
+    //     console.log('allCookiesMap', allCookiesMap);
+    //     });
+    // })
+
+    //console.log('allCookiesmap', allCookiesMap);
 })
